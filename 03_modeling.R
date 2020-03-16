56bd4a96a6a1dcefadb60f94d7fe512d15c29216
Will Bonnell <wtb3na@virginia.edu>
2020-03-16 16:30:28 -0500

    calculating distance by route for model covariate

--- 03_modeling.R
+++ 03_modeling.R
@@ -1,2 +1,8 @@
 # modeling lyft price
 
+library(data.table)
+
+uber_clean <- data.table::fread("uber_clean.csv")[1:80,]
+routes_df <- data.table::fread("routes_df.csv")
+
+distance <- aggregate(miles ~ .id, routes_df, FUN = sum)
